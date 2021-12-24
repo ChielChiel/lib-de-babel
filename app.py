c924dadd2f568b8960e027d676e7136cef3e2c7a
Michiel Schouten <51763911+ChielChiel@users.noreply.github.com>
2021-12-24 15:19:43 +0100

    changed filename

--- app.py
+++ app.py
@@ -1,54 +1,21 @@
 # app.py
-from flask import Flask, request, jsonify
-from libra import Library_de_Babel
+from flask import Flask, render_template, request, jsonify
+from libra.library_de_babel import Library_de_babel
 
 
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
+@app.route("/")
+@app.route("/<number>")
+def index(number = 20):
+    bib = Library_de_Babel(page_size=80 * 40)
+    number = int(number)
+    report = bib.creat_page(number)
+    # report = number
+    return render_template('page.html', name="La bibliotheca de Babel", output=report)
+    # return "<center><h1>La Bibliotheca de Babel</h1></center>"
 
-    # For debugging
-    print(f"got name {name}")
 
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
-
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+# @app.route("/librarian", methods=['GET'])
+# def librarian():
+#     return "huts"
--- libra/Library_de_Babel.py
+++ libra/Library_de_Babel.py
@@ -1,1 +1,87 @@
 print("dit is kaasje import")
+
+class Library_de_babel:
+    """sumary_line
+    
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+    
+    def __init__(self, page_size) -> None:
+        # self.alfabet = "abcdefghijklmnopqrstuvwxyz., "
+        self.alfabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '.', ',', ' ', '\n']
+        self.lookup_alfabet = {'a': 0, 'b': 1, 'c': 2,'d': 3,'e': 4,'f': 5,'g': 6,'h': 7,'i': 8,'j': 9,'k': 10,'l': 11,'m': 12,'n': 13,'o': 14,'p': 15,'q': 16,'r': 17,'s': 18,'t': 19,'u': 20,'v': 21,'w': 22,'x': 23,'y': 24,'z': 25,'.': 26,',': 27,' ': 28}
+        self.alfabet_size = 29 # len(self.alfabet)
+        self.page_size = page_size
+        return
+
+
+    """
+    [summary]
+        converts base-10 into a page
+    [args]
+        numb: int base-10
+        length: int
+    """
+    def creat_page(self, numb, length = None):
+        # print(numb)
+        if length == None:
+            length = self.page_size
+        result = self.nthnary(numb, lente=length)
+        # print("start:" + ''.join([alfabet[x] for x in k]) + ";end")
+        result = [x for y in (result[i:i+80] + [29] * (i < len(result) - 2) for i in range(0, len(result), 80)) for x in y]
+
+        return ''.join([self.alfabet[x] for x in result])
+
+    def FillZero(self, to, listt):
+        while len(listt) < to:
+            listt.append(0)
+        return listt
+
+    """
+    [summary]
+        converts int into base 29 number
+    [args]
+        n : base-10 number
+        lente : int number of wheels
+
+    [returns]
+        nums: [base-29]
+    """
+    def nthnary(self, n, lente=4):
+        if n == 0:
+            return self.FillZero(lente, [])
+        nums = []
+        while n:
+            n, r = divmod(n, self.alfabet_size)
+            nums.append(r)
+            # print("nums", nums)
+        nums = self.FillZero(lente, nums)
+        # return ''.join(reversed(nums))
+        nums.reverse()
+        
+        return nums #reversed(nums)
+
+
+    def arynth(self, input_base, output_base = 29):
+        arr = [int(i) for i in input_base]
+        arr.reverse()
+        n = 0
+        total = 0
+        for digit in arr:
+            total += digit * (output_base ** n)
+            n += 1
+        return total
+
+    def nth(self, input_list):
+        outputt = []
+        for digit in input_list:
+            outputt.append(self.arynth(digit))
+        return outputt    
+
+    def string_to_base(self, input_str):
+        mid_string = []
+        for s in input_str:
+            mid_string.append(str(self.lookup_alfabet[s]))
+        return mid_string
